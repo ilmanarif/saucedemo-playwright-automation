4a537e7730dd0ac6fb85b62bf53d860d5d4ee063
ilmanarif <ilman.arif@ogya.co.id>
2026-09-23 14:35:53 +0700

    feat: add new dashboard test case for allure report

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test.describe('Pengujian Halaman Dashboard / Produk', () => {
+test.describe('Pengujian Halaman Dashboard / Katalog Produk', () => {
 
-  test('Memastikan pengguna berhasil melihat daftar produk setelah login', async ({ page }) => {
+  test('Memastikan pengguna berhasil melihat katalog produk setelah login', async ({ page }) => {
     const loginPage = new LoginPage(page);
     
     await page.goto('/');
 
     await loginPage.login('standard_user', 'secret_sauce');
 
-    await expect(page).toHaveURL(/.*dashboard.html/);
+    const headingTitle = page.locator('h1');
+    await expect(headingTitle).toHaveText('Katalog Produk Demo');
     
-    // 4. Validasi elemen keranjang atau judul produk muncul
-    const productTitle = page.locator('.title');
-    await expect(productTitle).toHaveText('Products');
+    const addToCartButton = page.locator('#add-to-cart-item-1');
+    await expect(addToCartButton).toBeVisible();
   });
 
 });
