d2e90cf55dcd325ee3a3b1cc7f687bf7dd66939d
ilmanarif <ilman.arif@ogya.co.id>
2026-09-23 14:29:58 +0700

    feat: add new dashboard test case for allure report

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -6,13 +6,10 @@ test.describe('Pengujian Halaman Dashboard / Produk', () => {
   test('Memastikan pengguna berhasil melihat daftar produk setelah login', async ({ page }) => {
     const loginPage = new LoginPage(page);
     
-    // 1. Buka halaman utama / login
     await page.goto('/');
 
-    // 2. Lakukan login menggunakan kredensial yang valid
     await loginPage.login('standard_user', 'secret_sauce');
 
-    // 3. Validasi apakah berhasil masuk ke halaman produk/dashboard
     await expect(page).toHaveURL(/.*dashboard.html/);
     
     // 4. Validasi elemen keranjang atau judul produk muncul
